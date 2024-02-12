4a8e76f71d4155160666b28ca85527fcaa165ab0
roboboredom <57413665+roboboredom@users.noreply.github.com>
2024-02-12 18:28:36 -0500

    Update CPolicy.java

--- src/com/github/roboboredom/CSC251Project/CPolicy.java
+++ src/com/github/roboboredom/CSC251Project/CPolicy.java
@@ -1,36 +1,17 @@
 package com.github.roboboredom.CSC251Project;
 
+
+
 class CPolicy
 {
-    /* NOTE: This style of naming is an old
-     * convention called hungarian notation.
-     * It is considered outdated by some because
-     * most modern IDEs display type directly,
-     * but I prefer it because not only does it prevent
-     * name collisions, it allows you to see
-     * what type something is outside the IDE without browsing
-     * the code directly. It also allows you to "search" autocomplete
-     * by typing a prefix. I learned about it from
-     * messing around with the source code for Quake, and Source SDK 2013.
-     * 
-     * An example of a large codebase that makes use of hungarian
-     * notation is 
-     * 
-     * Key for this file:
-     *     i   int
-     *     d   double
-     *     b   boolean
-     *     s   string
-     *     e   enum instance
-     *     E   enum declaration
-     *     C   class declaration
-     *     m_  member of a class
-     *     o   class instance (object)
-     *     a_  array of
-     *     al_ arraylist of
-     * 
-     * https://en.wikipedia.org/wiki/Hungarian_notation
-     */
+     /* Private member variables */
+    final double F_dINSURANCE_POLICY_BASE_FEE = 600.0D;
+    final double F_dINSURANCE_POLICY_SMOKER_FEE = 100.0D;
+    final double F_dINSURANCE_POLICY_AGE_FEE = 75.0D;
+    final int F_iINSURANCE_POLICY_AGE_FEE_THRESHOLD = 50;
+    final double F_dINSURANCE_POLICY_BMI_FEE_THRESHOLD = 35.0D;
+    final double F_dINSURANCE_POLICY_BMI_FEE_MULTIPLIER = 20.0D;
+
     enum m_ESmokingStatus { SMOKER, NON_SMOKER }
     
     int m_iPolicyNumber;
@@ -42,6 +23,7 @@ class CPolicy
     double m_dPolicyHolderHeightInches;
     double m_dPolicyHolderWeightPounds;
 
+    /* Constructors: */
     CPolicy()
     {
         m_iPolicyNumber = 0;
@@ -49,6 +31,84 @@ class CPolicy
         m_sPolicyHolderFirstName = "Example First Name";
         m_sPolicyHolderLastName = "Example Last Name";
         m_iPolicyHolderAge = 0;
-        m_ePolicyHolderSmokingStatus = m_ESmokingStatus.
+        m_ePolicyHolderSmokingStatus = m_ESmokingStatus.NON_SMOKER;
+        m_dPolicyHolderHeightInches = 0.0D;
+        m_dPolicyHolderWeightPounds = 0.0D;
+    }
+
+    CPolicy
+    (
+        int iPolicyNumber,
+        String sProviderName,
+        String sPolicyHolderFirstName,
+        String sPolicyHolderLastName,
+        int iPolicyHolderAge,
+        m_ESmokingStatus ePolicyHolderSmokingStatus,
+        double dPolicyHolderHeightInches,
+        double dPolicyHolderWeightPounds
+    )
+    {
+        m_iPolicyNumber = iPolicyNumber;
+        m_sProviderName = sProviderName;
+        m_sPolicyHolderFirstName = sPolicyHolderFirstName;
+        m_sPolicyHolderLastName = sPolicyHolderLastName;
+        m_iPolicyHolderAge = iPolicyHolderAge;
+        m_ePolicyHolderSmokingStatus = ePolicyHolderSmokingStatus;
+        m_dPolicyHolderHeightInches = dPolicyHolderHeightInches;
+        m_dPolicyHolderWeightPounds = dPolicyHolderWeightPounds;
+    }
+    
+    /* Setter methods for private member variables: */
+    void fSetPolicyNumber(int iPolicyNumber) { m_iPolicyNumber = iPolicyNumber; }
+    void fSetProviderName(String sProviderName) { m_sProviderName = sProviderName; }
+    void fSetPolicyHolderFirstName(String sPolicyHolderFirstName) { m_sPolicyHolderFirstName = sPolicyHolderFirstName; }
+    void fSetPolicyHolderLastName(String sPolicyHolderLastName) { m_sPolicyHolderLastName = sPolicyHolderLastName; }
+    void fSetPolicyHolderAge(int iPolicyHolderAge) { m_iPolicyHolderAge = iPolicyHolderAge; }
+    void fSetPolicyHolderSmokingStatus(m_ESmokingStatus eSmokingStatus) { m_ePolicyHolderSmokingStatus = eSmokingStatus; }
+    void fSetPolicyHolderHeightInches(double dPolicyHolderHeightInches) { m_dPolicyHolderHeightInches = dPolicyHolderHeightInches; }
+    void fSetPolicyHolderWeightPounds(double dPolicyHolderWeightPounds) { m_dPolicyHolderWeightPounds = dPolicyHolderWeightPounds; }
+
+    /* Getter methods for private member variables: */
+    int fGetPolicyNumber() { return m_iPolicyNumber; }
+    String fGetProviderName() { return m_sProviderName; }
+    String fGetPolicyHolderFirstName() { return m_sPolicyHolderFirstName; }
+    String fGetPolicyHolderLastName() { return m_sPolicyHolderLastName; }
+    int fGetPolicyHolderAge() { return m_iPolicyHolderAge; }
+    m_ESmokingStatus fGetPolicyHolderSmokingStatus() { return m_ePolicyHolderSmokingStatus; }
+    double fGetPolicyHolderHeightInches() { return m_dPolicyHolderHeightInches; }
+    double fGetPolicyHolderWeightPounds() { return m_dPolicyHolderWeightPounds; }
+
+    /* Methods: */
+    double fCalculatePolicyHolderBMI()
+    {
+        /* Calculate the policy holder's BMI */
+        return m_dPolicyHolderWeightPounds * 703.0D / Math.pow(m_dPolicyHolderHeightInches, 2.0D);
+    }
+
+    double fCalculateInsurancePolicyPrice()
+    {
+        /* Intialize the price to the base fee */
+        double dInsurancePolicyPrice = F_dINSURANCE_POLICY_BASE_FEE;
+
+        /* Add the age fee if the policy holder's age is greater than the age fee threshold */
+        if (m_iPolicyHolderAge > F_iINSURANCE_POLICY_AGE_FEE_THRESHOLD)
+        {
+            dInsurancePolicyPrice += F_dINSURANCE_POLICY_AGE_FEE;
+        }
+
+        /* Add the smoker fee if the policy holder is a smoker */
+        if (m_ePolicyHolderSmokingStatus == m_ESmokingStatus.SMOKER)
+        {
+            dInsurancePolicyPrice += F_dINSURANCE_POLICY_SMOKER_FEE;
+        }
+
+        /* Calculate and add the BMI fee if the policy holder's BMI is greater than the BMI fee threshold */
+        double dPolicyHolderBMI = fCalculatePolicyHolderBMI();
+        if (dPolicyHolderBMI > F_dINSURANCE_POLICY_BMI_FEE_THRESHOLD)
+        {
+            dInsurancePolicyPrice += (dPolicyHolderBMI - F_dINSURANCE_POLICY_BMI_FEE_THRESHOLD) * F_dINSURANCE_POLICY_BMI_FEE_MULTIPLIER;
+        }
+
+        return dInsurancePolicyPrice;
     }
 }
