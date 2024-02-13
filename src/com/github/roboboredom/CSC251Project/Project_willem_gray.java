d21a1fa8a17610de6cff6ac7b51b6731d6d64647
roboboredom <57413665+roboboredom@users.noreply.github.com>
2024-02-12 21:05:23 -0500

    CPolicy works now

--- src/com/github/roboboredom/CSC251Project/Project_willem_gray.java
+++ src/com/github/roboboredom/CSC251Project/Project_willem_gray.java
@@ -1,10 +1,25 @@
 package com.github.roboboredom.CSC251Project;
 
-// put demo / driver class for the project here
+/**
+ * Demo / driver class for CPolicy.
+ * <ul>
+ *     <li>This class contains the main method.
+ *     </li>
+ * </ul>
+ * 
+ * @author Willem Dawson Gray
+ */
 public class Project_willem_gray
 {
+    /** Program entrypoint. 
+     * @param a_sArgs Command line arguments to the program
+    */
     public static void main(String[] a_sArgs)
     {
+        System.out.println("roark roark roark roark roark!!!!!!!!!!!!!!!");
 
+        CPolicy oTestPolicy = new CPolicy(123123123, "Maned Wolf Insurance Corp", "Jane", "Doe", 67, CPolicy.m_ESmokingStatus.SMOKER, 25000000.323, 2939393.339);
+        System.out.println("oTestPolicy.fCalculateInsurancePolicyPrice() = " + oTestPolicy.fCalculateInsurancePolicyPrice()); 
+        System.out.println("oTestPolicy.fCalculatePolicyHolderBMI() = " + oTestPolicy.fCalculatePolicyHolderBMI()); 
     }
 }
