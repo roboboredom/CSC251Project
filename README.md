bfbaa1001cf0d59e204dd6a08279d4de5800a5ef
roboboredom <57413665+roboboredom@users.noreply.github.com>
2024-03-18 22:03:37 -0400

    Minor change to generate-javadoc.bat

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # CSC251Project
-Repository for hosting the CSC251 Programming Project
+Repository for hosting the CSC251 Programming Project.
--- generate-javadoc.bat
+++ generate-javadoc.bat
@@ -1,3 +1,7 @@
+@echo off
+
+rem Run the javadoc command to generate a documentation website for the current java project. 
+rem Use "-private" arg to toggle the documentation for private items.
 javadoc ^
 -private ^
 -d .\doc ^
