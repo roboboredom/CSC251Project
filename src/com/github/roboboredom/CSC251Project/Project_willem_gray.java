441341b771704e8a01296531709d7f75cd75b7a0
roboboredom <57413665+roboboredom@users.noreply.github.com>
2024-03-19 00:28:02 -0400

    Perfectly working project part two! :smiley: (#4)

--- src/com/github/roboboredom/CSC251Project/Project_willem_gray.java
+++ src/com/github/roboboredom/CSC251Project/Project_willem_gray.java
@@ -1,5 +1,5 @@
 /*
-* This project uses the following naming convention ontop of the teacher required one: 
+* This project uses the following naming convention ontop of the teacher required one:
 * An old convention called hungarian notation.
 * It is considered outdated by some because most modern IDEs display type directly,
 * but I prefer it because not only does it prevent name collisions, it allows you to see
@@ -24,7 +24,7 @@
 * Prefixes can be combined in logical ways, such as:
 *     "m_al_bBlockState" class member, arraylist of booleans, named BlockState
 *
-* Basically, I like it because it makes the code just verbose enough to be easier for me to read quickly, 
+* Basically, I like it because it makes the code just verbose enough to be easier for me to read quickly,
 * without doing something crazy like: "ClassName_ArrayListOfBool_BlockState" or just relying on the IDE,
 * (which may or may not support type display).
 *
@@ -33,6 +33,9 @@
 
 package com.github.roboboredom.CSC251Project;
 
+import java.util.ArrayList;
+import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 
 /**
@@ -41,93 +44,132 @@ import java.util.Scanner;
  *     <li>This class contains the main method.
  *     </li>
  * </ul>
- * 
+ *
  * @author Willem Dawson Gray
  */
 public class Project_willem_gray
 {
-    /** Program entrypoint. 
+    /** Program entrypoint.
      * @param a_sArgs Command line arguments to the program
     */
     public static void main(String[] a_sArgs)
     {
-        /* Initialize a scanner and also a new policy object */
-        Scanner oScanner = new Scanner(System.in);
-        CPolicy oPolicy = new CPolicy();
+        /* Initialize an arraylist to hold our insurance policy objects. */
+        ArrayList<CPolicy> al_oPolicies= new ArrayList<CPolicy>();
 
-        /* Prompt the user to enter the required policy data */
-        /* There is NO input santization here, but the project did not say it was required. */
-        System.out.print("Please enter the Policy Number: ");
-        oPolicy.fSetPolicyNumber(oScanner.nextInt());
-        oScanner.nextLine();
-
-        System.out.print("Please enter the Provider Name: ");
-        oPolicy.fSetProviderName(oScanner.nextLine());
-        
-        System.out.print("Please enter the Policyholder's First Name: ");
-        oPolicy.fSetPolicyHolderFirstName(oScanner.nextLine());
-
-        System.out.print("Please enter the Policyholder's Last Name: ");
-        oPolicy.fSetPolicyHolderLastName(oScanner.nextLine());
-
-        System.out.print("Please enter the Policyholder's Age: ");
-        oPolicy.fSetPolicyHolderAge(oScanner.nextInt());
-        oScanner.nextLine();
-
-        System.out.println("Please enter the Policyholder's Smoking Status (smoker/non-smoker): ");
-        String sInput = oScanner.nextLine();
-        if (sInput.equals("smoker"))
-        {
-            oPolicy.fSetPolicyHolderSmokingStatus(CPolicy.m_ESmokingStatus.SMOKER);
-        }
-        else if (sInput.equals("non-smoker"))
-        {
-            oPolicy.fSetPolicyHolderSmokingStatus(CPolicy.m_ESmokingStatus.NON_SMOKER);
-        }
-
-        System.out.print("Please enter the Policyholder's Height (in inches): ");
-        oPolicy.fSetPolicyHolderHeightInches(oScanner.nextDouble());
-        oScanner.nextLine();
-
-        System.out.print("Please enter the Policyholder's Weight (in pounds): ");
-        oPolicy.fSetPolicyHolderWeightPounds(oScanner.nextDouble());
-        oScanner.nextLine();
-
-        /* Print out all the entered data */
-        System.out.println("\nPolicy Number: " + oPolicy.fGetPolicyNumber());
-        System.out.println("Provider Name: " + oPolicy.fGetProviderName());
-        System.out.println("Policyholder's First Name: " + oPolicy.fGetPolicyHolderFirstName());
-        System.out.println("Policyholder's Last Name: " + oPolicy.fGetPolicyHolderLastName());
-        System.out.println("Policyholder's Age: " + oPolicy.fGetPolicyHolderAge());
-
-        /* There is probably a better way of doing this, but this works okay. 
-         * I am talking about printing the name of the enum state. NOT if this should be a boolean.
-         * 
-         * This should be an enum, because the program may need to be extended to support other 
-         * smoking states later on, such as "ECIG_SMOKER, etc.", since this is insurance, and 
-         * different drug types that can be smoked have different risk factors that should
-         * affect the insurance policy cost 
+        /* Place file handling logic in a try-catch block so that any exceptions regarding files
+         * are caught and reported to the user instead of just exiting or crashing ambiguously.
          */
-        CPolicy.m_ESmokingStatus ePolicyHolderSmokingStatus = oPolicy.fGetPolicyHolderSmokingStatus();
-        if (ePolicyHolderSmokingStatus == CPolicy.m_ESmokingStatus.NON_SMOKER)
+        try
         {
-            System.out.println("Policyholder's Smoking Status: non-smoker");
+            /* Intialize a file handler. Set it to the Policyinformation.txt file.
+            * When passing in the filepath, use the OS-dependent file separator.
+            */
+            File oPolicyInformationFile = new File("assets" + File.separator + "policyinformation.txt");
+
+            /* Initialize a scanner; pass to it the file.
+            * Set it to use only the OS-dependent line separator
+            * sequence as the delimiter, instead of any whitespace.
+            */
+            Scanner oScanner = new Scanner(oPolicyInformationFile);
+            oScanner.useDelimiter(System.lineSeparator());
+
+            /* Scan in the items from the file. */
+            while(oScanner.hasNext())
+            {
+                int iPolicyNumber = Integer.parseInt(oScanner.next());
+                String sProviderName = oScanner.next();
+                String sPolicyHolderFirstName = oScanner.next();
+                String sPolicyHolderLastName = oScanner.next();
+                int iPolicyHolderAge = Integer.parseInt(oScanner.next());
+
+                CPolicy.m_ESmokingStatus ePolicyHolderSmokingStatus = CPolicy.m_ESmokingStatus.NON_SMOKER;
+                if (oScanner.next().equalsIgnoreCase("smoker"))
+                {
+                    ePolicyHolderSmokingStatus = CPolicy.m_ESmokingStatus.SMOKER;
+                }
+
+                double dPolicyHolderHeightInches = Double.parseDouble(oScanner.next());
+                double dPolicyHolderWeightPounds = Double.parseDouble(oScanner.next());
+
+                /* Don't read the BMI and price from the file, those can be calculated
+                 * at runtime from the above data for a tiny CPU cost so are redundant.
+                 */
+
+                /* It seems pointless to use these named buffer variables when 
+                 * you could read in and send the data inline below, but that 
+                 * would be a lot less readable. */
+                al_oPolicies.add(new CPolicy(
+                    iPolicyNumber,
+                    sProviderName,
+                    sPolicyHolderFirstName,
+                    sPolicyHolderLastName,
+                    iPolicyHolderAge,
+                    ePolicyHolderSmokingStatus,
+                    dPolicyHolderHeightInches,
+                    dPolicyHolderWeightPounds)
+                );
+
+                /* Skip any blank tokens if we have not reached the end of the file. */
+                if(oScanner.hasNext())
+                {
+                    oScanner.next();
+                }
+            }
+
+            /* Close the scanner to prevent a resource leak. */
+            oScanner.close();
         }
-        else
+        catch(IOException oIOException)
         {
-            System.out.println("Policyholder's Smoking Status: smoker");
+            /* Inform the user of the IOException error and what file it occured with. */
+            System.out.println("Error trying to read \"assets" + File.separator + "policyinformation.txt\": " + oIOException.getMessage());
         }
 
-        System.out.println("Policyholder's Height: " + String.format("%.1f", oPolicy.fGetPolicyHolderHeightInches()) + " inches");
-        System.out.println("Policyholder's Weight: " + String.format("%.1f", oPolicy.fGetPolicyHolderWeightPounds()) + " pounds");
+        /* Traverse through the list and print out the data for each insurance policy object.
+        * While doing so, keep track of the # of non-smokers & smokers and print the totals after.
+        */
+        int iNonSmokerCount = 0;
+        int iSmokerCount = 0;
 
-        /* Run the needed calculations and print out the BMI and total insurance policy price */
-        System.out.println("Policyholder's BMI: " + String.format("%.2f", oPolicy.fCalculatePolicyHolderBMI()));
-        System.out.println("Policy Price: $" + String.format("%.2f", oPolicy.fCalculateInsurancePolicyPrice()));
+        for (CPolicy oPolicy : al_oPolicies)
+        {
+            System.out.println("Policy Number: " + oPolicy.fGetPolicyNumber());
+            System.out.println("Provider Name: " + oPolicy.fGetProviderName());
+            System.out.println("Policyholder's First Name: " + oPolicy.fGetPolicyHolderFirstName());
+            System.out.println("Policyholder's Last Name: " + oPolicy.fGetPolicyHolderLastName());
+            System.out.println("Policyholder's Age: " + oPolicy.fGetPolicyHolderAge());
 
-        /* Close the scanner to prevent a resource leak */
-        oScanner.close();
+            /* There is probably a better way of doing this, but this works okay.
+            * I am talking about printing the name of the enum state. NOT if this should be a boolean.
+            *
+            * This should be an enum, because the program may need to be extended to support other
+            * smoking states later on, such as "ECIG_SMOKER, etc.", since this is insurance, and
+            * different drug types that can be smoked have different risk factors that should
+            * affect the insurance policy cost.
+            */
+            CPolicy.m_ESmokingStatus ePolicyHolderSmokingStatus = oPolicy.fGetPolicyHolderSmokingStatus();
+            if (ePolicyHolderSmokingStatus == CPolicy.m_ESmokingStatus.NON_SMOKER)
+            {
+                iNonSmokerCount++;
+                System.out.println("Policyholder's Smoking Status: non-smoker");
+            }
+            else
+            {
+                iSmokerCount++;
+                System.out.println("Policyholder's Smoking Status: smoker");
+            }
 
-        /* This program could loop, but the teacher did not say to use a read-eval-print-loop (REPL) structure for this particular project */
+            System.out.println("Policyholder's Height: " + String.format("%.1f", oPolicy.fGetPolicyHolderHeightInches()) + " inches");
+            System.out.println("Policyholder's Weight: " + String.format("%.1f", oPolicy.fGetPolicyHolderWeightPounds()) + " pounds");
+
+            /* Run the needed calculations and print out the BMI and total insurance policy price */
+            System.out.println("Policyholder's BMI: " + String.format("%.2f", oPolicy.fCalculatePolicyHolderBMI()));
+            System.out.println("Policy Price: $" + String.format("%.2f", oPolicy.fCalculateInsurancePolicyPrice()) + "\n"); // newline to space out each policies' data
+        }
+
+        /* Print the smoker totals */
+        System.out.println("The number of policies with a smoker is: " + iSmokerCount);
+        System.out.println("The number of policies with a non-smoker is: " + iNonSmokerCount);
     }
 }
